83b07eb645c9e3053f6cafd6948b781b374dae8c
Fredrik <fredde_lutz@hotmail.com>
2023-03-05 11:51:01 +0100

    exercise 3 extra credit 1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -2,13 +2,12 @@
 // http://localhost:3000/isolated/exercise/03.js
 
 import * as React from 'react'
-import {createContext, useState} from 'react'
+import {createContext, useState, useContext} from 'react'
 
 const CountContext = createContext()
 
 const CountProvider = ({children}) => {
   const [count, setCount] = useState(0)
-
   return (
     <CountContext.Provider value={{count, setCount}}>
       {children}
@@ -16,13 +15,20 @@ const CountProvider = ({children}) => {
   )
 }
 
+const useCount = () => {
+  const context = useContext(CountContext)
+  if (!context)
+    throw new Error('useCount can not be used outside of CountProvider')
+  return context
+}
+
 function CountDisplay() {
-  const {count} = React.useContext(CountContext)
+  const {count} = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const {setCount} = React.useContext(CountContext)
+  const {setCount} = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
